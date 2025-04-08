c81a6ab23bb67b44d6ab2514a043d4205f8d1043
emmam <emmamanukyan2006@gmail.com>
2025-04-08 10:32:38 +0400

    precise levels without inertia

--- src/main/java/frc/robot/commands/RollerCommand.java
+++ src/main/java/frc/robot/commands/RollerCommand.java
@@ -7,10 +7,16 @@ import frc.robot.subsystems.RollerSubsystem;
 public class RollerCommand extends Command {
     private final RollerSubsystem rollerSubsystem;
     private final XboxController controller;
-    private double targetRotations = -1;
 
-    private int isRightBumperPressed = 0;
-    private int isLeftBumperPressed = 0;
+    private int level_1 = 20;
+    private int level_2 = 60;
+    private int level_3 = 100;
+    private int level_4 = 140;
+
+    private int isButtonPressedLevel_1 = 0;
+    private int isButtonPressedLevel_2 = 0;
+    private int isButtonPressedLevel_3 = 0;
+    private int isButtonPressedLevel_4 = 0;
 
     public RollerCommand(RollerSubsystem subsystem, XboxController controller) {
         this.rollerSubsystem = subsystem;
@@ -22,79 +28,146 @@ public class RollerCommand extends Command {
     @Override
     public void execute() {
         int pov = controller.getPOV();
-        System.out.println("povvvvvvv  "+pov);
+        System.out.println("povvvvvvv  " + pov);
 
         System.out.println("Encoder valueeee   " + rollerSubsystem.getRotations());
-        if (isRightBumperPressed == 1 && rollerSubsystem.getRotations() < 20) {
-            if (rollerSubsystem.isAtHighLimit()) {
-                rollerSubsystem.setRollerSpeed(0);
-            } else {
-                rollerSubsystem.setRollerSpeed(0.7);
-            }
-        } else if (isRightBumperPressed == 2 && rollerSubsystem.getRotations() > 20) {
-            if (rollerSubsystem.isAtLowLimit()) {
-                rollerSubsystem.setRollerSpeed(0);
-            } else {
-                rollerSubsystem.setRollerSpeed(-0.7);
-            }
+
+        // level 1 check
+        if (isButtonPressedLevel_1 == 1 && rollerSubsystem.getRotations() < level_1) {
+            rollerSubsystem.setRollerPositiveSpeed(0.7);
+        } else if (isButtonPressedLevel_1 == 2 && rollerSubsystem.getRotations() > level_1) {
+            rollerSubsystem.setRollerNegativeSpeed(-0.7);
         } else {
-            isRightBumperPressed = 0;
+            isButtonPressedLevel_1 = 0;
         }
 
-        if (isLeftBumperPressed == 1 && rollerSubsystem.getRotations() < 60) {
-            if (rollerSubsystem.isAtHighLimit()) {
-                rollerSubsystem.setRollerSpeed(0);
-            } else {
-                rollerSubsystem.setRollerSpeed(0.7);
-            }
-        }else if (isRightBumperPressed == 2 && rollerSubsystem.getRotations() > 60) {
-            if (rollerSubsystem.isAtLowLimit()) {
-                rollerSubsystem.setRollerSpeed(0);
-            } else {
-                rollerSubsystem.setRollerSpeed(-0.7);
-            }
+        // level 2 check
+        if (isButtonPressedLevel_2 == 1 && rollerSubsystem.getRotations() < level_2) {
+            rollerSubsystem.setRollerPositiveSpeed(0.7);
+        } else if (isButtonPressedLevel_2 == 2 && rollerSubsystem.getRotations() > level_2) {
+            rollerSubsystem.setRollerNegativeSpeed(-0.7);
         } else {
-            isLeftBumperPressed = 0;
+            isButtonPressedLevel_2 = 0;
         }
+
+        // level 3 check
+        if (isButtonPressedLevel_3 == 1 && rollerSubsystem.getRotations() < level_3) {
+            rollerSubsystem.setRollerPositiveSpeed(0.7);
+        } else if (isButtonPressedLevel_3 == 2 && rollerSubsystem.getRotations() > level_3) {
+            rollerSubsystem.setRollerNegativeSpeed(-0.7);
+        } else {
+            isButtonPressedLevel_3 = 0;
+        }
+
+         // level 4 check
+         if (isButtonPressedLevel_4 == 1 && rollerSubsystem.getRotations() < level_4) {
+            rollerSubsystem.setRollerPositiveSpeed(0.7);
+        } else if (isButtonPressedLevel_4 == 2 && rollerSubsystem.getRotations() > level_4) { //////////////////?????????????????
+            rollerSubsystem.setRollerNegativeSpeed(-0.7);
+        } else {
+            isButtonPressedLevel_4 = 0;
+        }
+
 
         if (pov == 0) {
-            isLeftBumperPressed = 0;
-            if (rollerSubsystem.getRotations() > 0 && rollerSubsystem.getRotations() < 20) {
-                isRightBumperPressed = 1;
-            } else if (rollerSubsystem.getRotations() >=20) {
-                isRightBumperPressed = 2;
+            // level 1
+            resetButtonsPressed(); // ?
+            if (rollerSubsystem.getRotations() < level_1) {
+                isButtonPressedLevel_1 = 1;
+            } else if (rollerSubsystem.getRotations() >= level_1) {
+                isButtonPressedLevel_1 = 2;
             }
 
-        } else if (controller.getLeftBumperButton()) {
-            isLeftBumperPressed = 0;
-            isRightBumperPressed = 0;
-            if (rollerSubsystem.isAtHighLimit()) {
-                rollerSubsystem.setRollerSpeed(0);
-            } else {
-                rollerSubsystem.setRollerSpeed(1);
-            }
-        } else if (pov==90) {
-            if (rollerSubsystem.getRotations() > 0 && rollerSubsystem.getRotations() < 60) {
-                isLeftBumperPressed = 1;
-            } else if (rollerSubsystem.getRotations() > 60) {
-                isLeftBumperPressed = 2;
-            }
-            isRightBumperPressed = 0;
+        }
 
-        } else if (controller.getRightBumperButton()) {
-            isLeftBumperPressed = 0;
-            isRightBumperPressed = 0;
-            if (rollerSubsystem.isAtLowLimit()) {
-                rollerSubsystem.setRollerSpeed(0);
-            } else {
-                rollerSubsystem.setRollerSpeed(-1);
+        else if (pov == 90) {
+            // level 2
+            resetButtonsPressed(); // ?
+            if (rollerSubsystem.getRotations() > 0 && rollerSubsystem.getRotations() < level_2) {
+                isButtonPressedLevel_2 = 1;
+            } else if (rollerSubsystem.getRotations() > level_2) {
+                isButtonPressedLevel_2 = 2;
             }
-        } else {
-            if (isLeftBumperPressed == 0 && isRightBumperPressed == 0) {
+            // isButtonPressedLevel_1 = 0;
+        }
+
+        else if (pov == 180) {
+            // level 3
+            resetButtonsPressed(); // ?
+            if (rollerSubsystem.getRotations() > 0 && rollerSubsystem.getRotations() < level_3) {
+                isButtonPressedLevel_3 = 1;
+            } else if (rollerSubsystem.getRotations() > level_3) {
+                isButtonPressedLevel_3 = 2;
+            }
+            // isButtonPressedLevel_1 = 0;
+        }
+
+        else if (pov == 270) {
+            // level 4
+            resetButtonsPressed(); // ?
+            if (rollerSubsystem.getRotations() > 0 && rollerSubsystem.getRotations() < level_4) {
+                isButtonPressedLevel_4 = 1;
+            } else if (rollerSubsystem.getRotations() > level_4) {
+                isButtonPressedLevel_4 = 2;
+            }
+            // isButtonPressedLevel_1 = 0;
+        }
+
+        else if (controller.getLeftBumperButton()) {
+            resetButtonsPressed();
+            rollerSubsystem.moveToPosition(rollerSubsystem.getRotations() + 10,true); // quick move up
+        }
+
+        else if (controller.getRightBumperButton()) {
+            resetButtonsPressed();
+            rollerSubsystem.moveToPosition(rollerSubsystem.getRotations() - 10, false); // quick move down
+        }
+
+        else {
+            if (isNoneOfTheButtonsPressed()) {
                 rollerSubsystem.setRollerSpeed(0);
             }
         }
 
+        // switch (pov) {
+        //     case 0:
+        //         rollerSubsystem.moveToPosition(level_1);
+        //         break;
+        //     case 90:
+        //         rollerSubsystem.moveToPosition(level_2);
+        //         break;
+        //     case 180:
+        //         rollerSubsystem.moveToPosition(level_3);
+        //         break;
+        //     case 270:
+        //         rollerSubsystem.moveToPosition(level_4);
+        //         break;
+        //     default:
+        //         if (!controller.getLeftBumper() && !controller.getRightBumper()) {
+        //             rollerSubsystem.stop();
+        //         }
+        //         break;
+        // }
+
+        // if (controller.getLeftBumper()) {
+        //     rollerSubsystem.moveToPosition(rollerSubsystem.getRotations() + 10); // quick move up
+        // }
+
+        // if (controller.getRightBumper()) {
+        //     rollerSubsystem.moveToPosition(rollerSubsystem.getRotations() - 10); // quick move down
+        // }
+
+    }
+
+    public void resetButtonsPressed() {
+        isButtonPressedLevel_1 = 0;
+        isButtonPressedLevel_2 = 0;
+        isButtonPressedLevel_3 = 0;
+        isButtonPressedLevel_4 = 0;
+    }
+
+    public boolean isNoneOfTheButtonsPressed() {
+        return isButtonPressedLevel_1 == 0 && isButtonPressedLevel_2 == 0 && isButtonPressedLevel_3==0 && isButtonPressedLevel_4==0;
     }
 }
 
--- src/main/java/frc/robot/subsystems/RollerSubsystem.java
+++ src/main/java/frc/robot/subsystems/RollerSubsystem.java
@@ -1,8 +1,15 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkClosedLoopController;
 
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -11,15 +18,56 @@ public class RollerSubsystem extends SubsystemBase {
     private final SparkMax rollerMotor = new SparkMax(5, MotorType.kBrushless);
     private final RelativeEncoder encoder = rollerMotor.getEncoder();
 
+    SparkClosedLoopController pidController = rollerMotor.getClosedLoopController();
+
     private final DigitalInput limitLowSwitch = new DigitalInput(0); // Change 0 to the correct port
     private final DigitalInput limitHighSwitch = new DigitalInput(1); // Change 0 to the correct port
 
     public RollerSubsystem() {
+        SparkMaxConfig config = new SparkMaxConfig();
+        config.inverted(false) // Set according to your setup
+                .idleMode(SparkMaxConfig.IdleMode.kBrake).encoder
+                .positionConversionFactor(1.0) // Adjust as needed
+                .velocityConversionFactor(1.0); // Adjust as needed
+        config.closedLoop
+                .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
+                .pid(0.1, 0.0, 0.0); // Initial PID values; tune as necessary
+
+        // Apply the configuration to the motor
+        rollerMotor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+        // Retrieve the PID controller after configuration
+        pidController = rollerMotor.getClosedLoopController();
+
         encoder.setPosition(0);
+    }
+
+    public void moveToPosition(double targetRotations, boolean isUp) {
+        if (isUp && isAtHighLimit() || !isUp && isAtLowLimit()) {
+            setRollerSpeed(0);
+        }else {
+            pidController.setReference(targetRotations, ControlType.kPosition);
+        }
     }
 
     public void setRollerSpeed(double speed) {
         rollerMotor.set(speed);
+    }
+
+    public void setRollerPositiveSpeed(double speed) {
+        if (isAtHighLimit()) {
+            setRollerSpeed(0);
+        } else {
+            setRollerSpeed(speed);
+        }
+    }
+
+    public void setRollerNegativeSpeed(double speed) {
+        if (isAtLowLimit()) {
+            setRollerSpeed(0);
+        } else {
+            setRollerSpeed(speed);
+        }
     }
 
     public boolean isAtLowLimit() {
@@ -35,12 +83,11 @@ public class RollerSubsystem extends SubsystemBase {
     }
 
     public void resetEncoder() {
-        encoder.setPosition(0); 
+        encoder.setPosition(0);
     }
 
     public void stop() {
         rollerMotor.set(0);
     }
-
 
 }
