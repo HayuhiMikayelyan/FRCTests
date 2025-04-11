406d54cfda082655de6497b12b319b360d045281
Edmond2007 <edmondseropyan6@gmail.com>
2025-04-11 11:04:13 +0400

    +speeeed

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -35,8 +35,8 @@ public class DriveSubsystem extends SubsystemBase {
     }
 
     public void setMotorSpeeds(double leftFrontSpeed, double leftRearSpeed, double rightFrontSpeed, double rightRearSpeed) {
-        double driveLeft = 2;
-        double driveRight = 2;
+        double driveLeft = 1.5;
+        double driveRight = 1.5;
         leftFront.set(leftFrontSpeed/driveLeft);
         leftRear.set(leftRearSpeed/driveLeft);
         rightFront.set(rightFrontSpeed/(-driveRight));
